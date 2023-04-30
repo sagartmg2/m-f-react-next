fb13959b77a15a1e39a5c425368e19e67bbce9bc
Sagar <dev.sagartmg@gmail.com>
2023-04-30 17:14:34 +0545

    init ecommerce

---
+++ ecommerce/src/pages/index.jsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+
+export default function Home({user}) {
+  return (
+    <>
+     <header className='bg-primary text-white'>
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>poruduct</li>
+        </ul>
+      </nav>
+
+     </header>
+    </>
+  )
+}
