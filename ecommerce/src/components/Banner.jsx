5a5de7a661290e96d5b9c83b0a59c577ff3bb90a
Sagar <dev.sagartmg@gmail.com>
2023-05-03 17:08:10 +0545

    featured products and grid

---
+++ ecommerce/src/components/Banner.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Image from 'next/image'
+import BannerImage from "@/assets/promotional.png"
+
+export default function Banner() {
+    return (
+        <>
+            {/* <div className="relative">
+        <Image src={Banner} className='w-full' />
+        <div className="absolute top-0">
+          <div className="container">
+            <p className='text-secondary font-bold' >Best Furniture For Your Castle....</p>
+            <h1 className='text-5xl font-bold'>New Furniture Collection
+              Trends in 2020</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
+              in phasellus non in justo.
+            </p>
+            <button className='bg-secondary text-white py-5 px-7'>
+              Shop Now
+            </button>
+          </div>
+
+        </div>
+      </div> */}
+            <div className=' py-48 bg-[url("../assets/promotional.png")] bg-cover bg-no-repeat  '>
+                <div className="container">
+                    <div className="w-1/2">
+                        <p className='text-secondary font-bold mb-3' >Best Furniture For Your Castle....</p>
+                        <h1 className='text-5xl font-bold mb-3'>New Furniture Collection
+                            Trends in 2020</h1>
+                        <p className='mb-3'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
+                            in phasellus non in justo.
+                        </p>
+                        <button className='bg-secondary text-white py-3 px-6'>
+                            Shop Now
+
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- ecommerce/src/pages/index.jsx
+++ ecommerce/src/pages/index.jsx
@@ -1,14 +1,60 @@
-import Image from 'next/image'
-import Banner from "@/assets/promotional.png"
-import Header from '@/components/Header'
 
+import Banner from '@/components/Banner'
+import Header from '@/components/Header'
+import Image from 'next/image'
+import ChairImg from "@/assets/chair.png"
 
 
 export default function Home({ user }) {
   return (
     <>
       <Header />
-      <Image src={Banner} />
+      <Banner />
+      <div className='mt-32 container'>
+        <h2 className='text-4xl font-semibold text-header text-center mb-12'>Featured Products</h2>
+        <div className='flex flex-wrap justify-between'>
+          <div className='border shadow basis-[270px] flex-grow  '>
+            <Image src={ChairImg} className='bg-[#F6F7FB] w-full ' height={200} width={200} w-full />
+            <div className='text-center'>
+              <p className='text-secondary text-xl'>Cantilever chair</p>
+              <p className='text-header'>code: XYZ</p>
+              <p className='text-header'>$ 100</p>
+            </div>
+          </div>
+          <div className='border shadow basis-[270px] flex-grow '>
+            <Image src={ChairImg} className='bg-[#F6F7FB] w-full ' height={200} width={200} w-full />
+            <div className='text-center'>
+              <p className='text-secondary text-xl'>Cantilever chair</p>
+              <p className='text-header'>code: XYZ</p>
+              <p className='text-header'>$ 100</p>
+            </div>
+          </div>
+          <div className='border shadow basis-[270px] flex-grow '>
+            <Image src={ChairImg} className='bg-[#F6F7FB] w-full ' height={200} width={200} w-full />
+            <div className='text-center'>
+              <p className='text-secondary text-xl'>Cantilever chair</p>
+              <p className='text-header'>code: XYZ</p>
+              <p className='text-header'>$ 100</p>
+            </div>
+          </div>
+          <div className='border shadow basis-[270px] flex-grow '>
+            <Image src={ChairImg} className='bg-[#F6F7FB] w-full ' height={200} width={200} w-full />
+            <div className='text-center'>
+              <p className='text-secondary text-xl'>Cantilever chair</p>
+              <p className='text-header'>code: XYZ</p>
+              <p className='text-header'>$ 100</p>
+            </div>
+          </div>
+          <div className='border shadow basis-[270px] flex-grow '>
+            <Image src={ChairImg} className='bg-[#F6F7FB] w-full ' height={200} width={200} w-full />
+            <div className='text-center'>
+              <p className='text-secondary text-xl'>Cantilever chair</p>
+              <p className='text-header'>code: XYZ</p>
+              <p className='text-header'>$ 100</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
