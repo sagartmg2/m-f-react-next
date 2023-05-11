1a0e484c68bf12a82f553f396cfee6e7704c5f3b
Sagar <dev.sagartmg@gmail.com>
2023-05-11 18:06:22 +0545

    form validation

--- ecommerce/src/pages/signup.jsx
+++ ecommerce/src/pages/signup.jsx
@@ -1,23 +1,74 @@
 import Header from '@/components/Header'
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Signup() {
 
+  let [name, setName] = useState("")
+  let [email, setEmail] = useState("")
+
+  // let [errors, setErros] = useState([])
+  let [error, setErros] = useState({
+
+  })
+
   function handleSubmit(event) {
+
+
+
     event.preventDefault()
 
-    axios.post("https://ecommerce-sagartmg2.vercel.app/api/users/signup", {
-      "name": "",
-      "role": "",
-      "password": "password",
-      "email": "ram@ram1.com"
-    })
-      .catch(err => {
-        console.log(err)
+    /* 
+      check if our form datas are valid
+      
+
+    */
+
+    let temp = {} // temp  = { name:"required",email:"required" }
+    let validation = true;
+
+    if (!name) {
+      temp.name = "required"
+      validation = false
+    }
+    if (!email) {
+      temp.email = "required field"
+      validation = false;
+    }
+
+    setErros(temp)
+
+    if (validation) {
+      axios.post("https://ecommerce-sagartmg2.vercel.app/api/users/signup", {
+        "name": name,
+        "role": event.target.role.value,
+        "password": event.target.password.value,
+        "email": email
       })
+        .then(res => {
+          alert("user created.")
+        })
+        .catch(err => {
+          console.log(err)
+          // let arr = res.data.errors
+          let temp = {}
+          if (err.response.data.errors && err.response.data.errors?.length > 0) {
+            err.response.data.errors.forEach(individual_error => {
+              temp[individual_error.param] = individual_error.msg
+            })
+
+            setErros(temp)
+          }
+        })
+    }
+
+
+
+
 
   }
+
+  console.log("render...");
 
   return (
     <>
@@ -25,22 +76,55 @@ export default function Signup() {
 
       <form className='container' onSubmit={handleSubmit}  >
         <div class="mb-6">
-          <label for="email" class="form-label">Name</label>
-          <input type="email" id="email" class="form-control" placeholder="name@flowbite.com" />
+          <label htmlFor="email" class="form-label">Name</label>
+          <input type="text" name="name" value={name} onChange={(e) => {
+            setName(e.target.value)
+
+            if (e.target.value) {
+              setErros({ ...error, name: "" })
+            } else {
+              setErros({ ...error, name: "required" })
+
+            }
+
+          }} class="form-control" placeholder="" />
+          {
+            error.name
+            &&
+            <small className='text-red-500'>{error.name}</small>
+          }
         </div>
         <div class="mb-6">
-          <label for="email" class=" form-label">Your email</label>
-          <input type="email" id="email" className="form-control" placeholder="name@flowbite.com" />
+          <label htmlFor="email" class=" form-label">Your email</label>
+          <input type="email" name='email' value={email} onChange={(e) => {
+            setEmail(e.target.value)
+            if (e.target.value) {
+              setErros({ ...error, email: "" })
+            } else {
+              setErros({ ...error, email: "required" })
+
+            }
+          }} id="email" className="form-control" placeholder="name@flowbite.com" />
+          {
+            error.email
+            &&
+            <small className='text-red-500'>{error.email}</small>
+          }
         </div>
         <div class="mb-6">
-          <label for="password" class="form-label">Your password</label>
-          <input type="password" id="password" class="form-control" />
+          <label htmlFor="password" class="form-label">Your password</label>
+          <input type="password"  name='password' id="password" class="form-control" />
+          {
+            error.password
+            &&
+            <small className='text-red-500'>{error.password}</small>
+          }
         </div>
         <div class="mb-6">
-          <label for="password" class="form-label">Role</label>
-          <select className='form-control'>
+          <label htmlFor="password" class="form-label">Role</label>
+          <select className='form-control' name='role'  >
             <option>Select</option>
-            <option>Buyer</option>
+            <option selected value={"buyer"}>Buyer</option>
             <option>Seller</option>
           </select>
         </div>
